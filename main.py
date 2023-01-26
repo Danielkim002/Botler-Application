02476185d242497cdcabcc3fffa3e66623e96654
Danielkim002 <danielskim02dsk@gmail.com>
2023-01-25 19:29:38 -0800

    implemented interactivity with application

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def botler():
     return render_template('botler.html')
-
+'''
 @app.route('/settings', methods=["POST"])
 def settings():
     css_file = 'botler_settings.css'
     if request.form.get("settings"):
         return render_template('botler_settings.html', css_file=css_file)
+ '''   
+@app.route('/settings', methods=["POST"])
+def settings():
+    css_file = 'botler_settings.css'
+    user_data = {}
+    if request.form.get("settings"):
+        with open("user_data.txt", "r") as f:
+            for line in f:
+                key, value = line.strip().split(":")
+                user_data[key] = value
+        return render_template('botler_settings.html', css_file=css_file, user_data=user_data)
+
+@app.route("/result", methods=["POST"])
+def result():
+    with open("user_data.txt") as f:
+        user_data = f.read()
     
+    user_data += request.form.get('text-box')
+    selected_value = request.form.get('dropdown')
+    if selected_value == "1":
+        user_data += "selected option was 1"
+    elif selected_value == "2":
+        user_data += "selected option was 2"
+    
+    return render_template('result.html',user_data = user_data)
+
+@app.route('/ret', methods=['POST'])
+def ret():
+    return redirect('/')
+
 @app.route('/save', methods=['POST'])
 def save():
     name = request.form.get('name')
@@ -22,12 +51,12 @@ def save():
 
     # Save the data to the user's computer
     with open("user_data.txt", "w") as f:
-        f.write(f"Name: {name}\n")
-        f.write(f"Academic History: {academic_history}\n")
-        f.write(f"Experience: {experience}\n")
-        f.write(f"Interests: {interests}\n")
-        f.write(f"Facts to Consider: {facts_to_consider}\n")
-    return "Data saved successfully!"
+        f.write(f"Name:{name}\n")
+        f.write(f"Academic History:{academic_history}\n")
+        f.write(f"Experience:{experience}\n")
+        f.write(f"Interests:{interests}\n")
+        f.write(f"Facts to Consider:{facts_to_consider}\n")
+    return redirect('/')
     
 if __name__ == '__main__':
     app.run(debug=True)
