19f477e5575a6ada51356894a8d29258db42ce2b
Danielkim002 <danielskim02dsk@gmail.com>
2023-01-26 20:41:17 -0800

    began implementing API

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, redirect
+import datetime
 
+import openai
+openai.api_key = "YOUR_API_KEY"
 app = Flask(__name__)
 
 @app.route('/')
@@ -23,18 +26,39 @@ def settings():
                 user_data[key] = value
         return render_template('botler_settings.html', css_file=css_file, user_data=user_data)
 
+
+'''
+response = openai.Completion.create(
+    prompt='Write a cover letter for a job in data science',
+    model='text-davinci-002',
+    temperature=0.5, #this is the creativity of the generated text, from 0 dry cardboard to 1 wedding cake
+    max_tokens=3000, #this paramater is the length of the generated text
+    top_p=1, #this paramer helps generate more coherent and meaninful text at the detriment of creativity 0 uncoherent very creative 1 not as creative but more coherent
+    frequency_penalty=1, #this parameter penalizes tokens that are similar even if they have high penatly, 0 no penalty for repeats, 1 generate less frequent and rarer tokens
+
+    presence_penalty=1  # 0 dont penalize for generating tokens present in input, 1 penalize for generating tokens present in input
+)
+
+generated_text = response.choices[0].text
+'''
 @app.route("/result", methods=["POST"])
 def result():
     with open("user_data.txt") as f:
         user_data = f.read()
     
     user_data += request.form.get('text-box')
+    text_box = request.form.get('text-box')
     selected_value = request.form.get('dropdown')
     if selected_value == "1":
         user_data += "selected option was 1"
     elif selected_value == "2":
         user_data += "selected option was 2"
     
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open("generated_response.txt", "a") as f:
+        f.write(f"[{current_time}]\n")
+        f.write(f"[text_box] \n{text_box}\n\n")
+        f.write(f"[user_data]\n {user_data}\n\n=============================================\n")
     return render_template('result.html',user_data = user_data)
 
 @app.route('/ret', methods=['POST'])
